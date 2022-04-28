5bf75d871aa8fbdf2ddda790b5ab7174b7a4a8bc
Gohar <gohar.mamunts@gmail.com>
2022-04-28 19:29:16 +0400

    adds high level logic

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,13 +2,16 @@ import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const [inputValue, setInputValue] = useState("");
+  const [operand1, setOperand1] = useState("");
+  const [operator, setOperator] = useState("");
+  const [operand2, setOperand2] = useState("");
+  const [result, setResult] = useState("");
 
   const renderDigits = () => {
     const digits = [];
     for (let i = 1; i < 10; i++) {
       digits.push(
-        <button onClick={handleDigitBtnClick} key={i} className="grid-item">
+        <button onClick={handleNumberBtnClick} key={i} className="grid-item">
           {i}
         </button>
       );
@@ -16,46 +19,106 @@ const App = () => {
     return digits;
   };
 
-  const updateInputValue = (e) => {
-    console.log("e.target.innerText", e.target.innerText);
-
-    setInputValue(e.target.innerText);
-    console.log("inputValue", inputValue);
+  const handleNumberBtnClick = (e) => {
+    if (!operand1) {
+      setOperand1((operand1) => operand1 + e.target.innerText);
+    }
+    if (operator) {
+      setOperand2((operand2) => operand2 + e.target.innerText);
+    }
   };
 
-  const handleInputChange = (e) => {
-    // console.log("inputValue", inputValue);
-    // console.log("e.target.innerText", e.target.innerText);
-    if (inputValue.includes(".") && e.target.innerText === ".") {
+  const handleOperetorBtnClick = (e) => {
+    // console.log("operand1", operand1);
+    if (result) {
+      setOperand1(result);
+    }
+
+    if (operator) {
+      setOperator(e.target.innerText);
+    }
+    if (operand1) {
+      setOperator(e.target.innerText);
+    }
+    //  else if (operand1 && operator) {
+    //   setOperator(e.target.innerText);
+    // }
+    if (operand1 && operator && operand2) {
+      setResult(equals(operator, operand1, operand2));
+    } else {
       return;
     }
   };
 
-  const handleOperatorBtnClick = () => {};
-  const handleDigitBtnClick = () => {};
-  const handleEqualsBtnClick = () => {};
+  const handlePeriodBtnClick = (e) => {
+    if (!operand1 || operand1.includes(".")) {
+      return;
+    } else {
+      setOperand1((operand1) => operand1 + e.target.innerText);
+    }
 
+    if (!operand2 || operand2.includes(".")) {
+      return;
+    } else {
+      setOperand2((operand2) => operand2 + e.target.innerText);
+    }
+  };
+
+  const handleEqualBtnClick = () => {
+    // console.log("operand1", operand1);
+    // console.log("result", result);
+    if (operator && operand1 && operand2) {
+      setResult(equals(operator, operand1, operand2));
+      // setOperand1(result);
+    } else {
+      return;
+    }
+  };
+
+  const equals = (operator, operand1, operand2) => {
+    let result;
+    switch (operator) {
+      case "/":
+        result = String(parseFloat(operand1) / parseFloat(operand2));
+        break;
+
+      case "x":
+        result = String(parseFloat(operand1) * parseFloat(operand2));
+        break;
+
+      case "+":
+        result = String(parseFloat(operand1) + parseFloat(operand2));
+        break;
+
+      case "-":
+        result = String(parseFloat(operand1) - parseFloat(operand2));
+        break;
+      default:
+        return;
+    }
+    return result;
+  };
   return (
     <div className="container">
       <div className="display">
         <input
           placeholder="0"
           type="text"
-          value={inputValue}
-          onChange={updateInputValue}
+          value={result}
+          //  onChange={result}
         />
       </div>
       <div className="operators">
-        <button onClick={handleOperatorBtnClick}>+</button>
-        <button onClick={handleOperatorBtnClick}>-</button>
-        <button onClick={handleOperatorBtnClick}>x</button>
-        <button onClick={handleOperatorBtnClick}>/</button>
+        <button onClick={handleOperetorBtnClick}>+</button>
+        <button onClick={handleOperetorBtnClick}>-</button>
+        <button onClick={handleOperetorBtnClick}>x</button>
+        <button onClick={handleOperetorBtnClick}>/</button>
       </div>
       <div className="grid-container">
         {renderDigits()}
-        <button onClick={handleDigitBtnClick}>0</button>
-        <button onClick={handleInputChange}>.</button>
-        <button onClick={handleEqualsBtnClick}>=</button>
+        <button onClick={handleNumberBtnClick}>0</button>
+        <button onClick={handlePeriodBtnClick}>.</button>
+        <button onClick={handleEqualBtnClick}>=</button>
       </div>
     </div>
   );
